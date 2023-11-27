b31ee7a60510125b9aa8c82c3159828c0a14f914
rami950 <s8mamusl@stud.uni-saarland.de>
2023-11-27 22:38:02 +0100

    greatest common divisor

--- src/unit_01.rs
+++ src/unit_01.rs
@@ -14,3 +14,31 @@ pub(crate) fn prints() {
     println!("Hello {:?}", ("world", 5)); // debug
     println!("Hello {:#?}", ("world", 5)); // pretty-print
 }
+
+/* Parameters
+Identifier |     |      Return Type
+     |     |     |          |
+    --- ------  ------     ---
+ fn mul(x: i32, y: i32) -> i32 {
+ --     -  ---          --     -
+ |      |   |            |     |
+Keyword |  Type         return |
+        |                      |
+    Identifier            Begin Function
+                              Body
+ */
+pub(crate) fn mul(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+//Recursion
+//In Rust you can call functions recursively, 
+//just like in this function for computing the greatest common divisor using Euclid’s algorithm:
+pub(crate) fn gcd(m: i32, n: i32) -> i32 {
+    if m == 0 {
+       n.abs()
+    } else {
+       gcd(n % m, m)
+    }
+ }
+ 
